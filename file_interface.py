be8886560d6fe338f88f8b278d3ba5bf570cb393
unknown <hnazila71@gmail.com>
2024-06-30 15:43:18 +0700

    Hasan

---
+++ file_interface.py
@@ -0,0 +1,52 @@
+import os
+import json
+import base64
+from glob import glob
+
+
+class FileInterface:
+    def __init__(self):
+        os.chdir('files/')
+
+    def list(self,params=[]):
+        try:
+            filelist = glob('*.*')
+            return dict(status='OK',data=filelist)
+        except Exception as e:
+            return dict(status='ERROR',data=str(e))
+
+    def get(self,params=[]):
+        try:
+            filename = params[0]
+            if (filename == ''):
+                return None
+            fp = open(f"{filename}",'rb')
+            isifile = base64.b64encode(fp.read()).decode()
+            return dict(status='OK',data_namafile=filename,data_file=isifile)
+        except Exception as e:
+            return dict(status='ERROR',data=str(e))
+
+    def upload(self, params=[]):
+        try:
+            filename = params[0]
+            filedata = params[1]
+            mode = 'ab' if os.path.exists(filename) else 'wb'  # Append if file exists
+            with open(filename, mode) as fp:
+                fp.write(base64.b64decode(filedata))
+            return dict(status='OK')
+        except Exception as e:
+            return dict(status='ERROR', data=str(e))
+
+    def delete(self, params=[]):
+        try:
+            filename = params[0]
+            os.remove(filename)
+            return dict(status='OK', data=f"{filename} berhasil dihapus")
+        except Exception as e:
+            return dict(status='ERROR', data=str(e))
+
+
+if __name__=='__main__':
+    f = FileInterface()
+    print(f.list())
+    print(f.get(['pokijan.jpg']))
